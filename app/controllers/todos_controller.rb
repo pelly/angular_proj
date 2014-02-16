bb905128d34b7afa425ea141050b28a3703e2ced
jondayton <jondayton@gmail.com>
2014-02-16 02:18:52 -0500

    rails controller to create new todos

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,4 +2,9 @@ class TodosController < ApplicationController
   def index
     render :json => Todo.all
   end
+
+  def create
+    new_todo = Todo.create(params.permit([:summary,:status]))
+    render :json => new_todo
+  end
 end
